b801866b30f91585f6a72a087b6339e93085f6b4
jaseemuddin <njaseemuddin@gmail.com>
2024-05-19 12:34:53 +0530

    updated

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,181 @@
+import React, { useEffect, useRef } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import { SiGmail, SiLinkedin, SiInstagram, SiX, SiDiscord, SiTelegram, SiFacebook, SiYoutube, SiMedium } from 'react-icons/si';
+
+const NO_CLIP = "polygon(0 0, 100% 0, 100% 100%, 0% 100%)";
+const BOTTOM_RIGHT_CLIP = "polygon(0 0, 100% 0, 0 0, 0% 100%)";
+const TOP_RIGHT_CLIP = "polygon(0 0, 0 100%, 100% 100%, 0% 100%)";
+const BOTTOM_LEFT_CLIP = "polygon(100% 100%, 100% 0, 100% 100%, 0 100%)";
+const TOP_LEFT_CLIP = "polygon(0 0, 100% 0, 100% 100%, 100% 0)";
+
+const ENTRANCE_KEYFRAMES = {
+  left: [BOTTOM_RIGHT_CLIP, NO_CLIP],
+  bottom: [BOTTOM_RIGHT_CLIP, NO_CLIP],
+  top: [BOTTOM_RIGHT_CLIP, NO_CLIP],
+  right: [TOP_LEFT_CLIP, NO_CLIP],
+};
+
+const EXIT_KEYFRAMES = {
+  left: [NO_CLIP, TOP_RIGHT_CLIP],
+  bottom: [NO_CLIP, TOP_RIGHT_CLIP],
+  top: [NO_CLIP, TOP_RIGHT_CLIP],
+  right: [NO_CLIP, BOTTOM_LEFT_CLIP],
+};
+
+const LinkBox = ({ Icon, href }) => {
+  const scope = useRef(null);
+  const animate = useAnimation();
+
+  const getNearestSide = (e) => {
+    const box = e.target.getBoundingClientRect();
+
+    const proximityToLeft = {
+      proximity: Math.abs(box.left - e.clientX),
+      side: "left",
+    };
+    const proximityToRight = {
+      proximity: Math.abs(box.right - e.clientX),
+      side: "right",
+    };
+    const proximityToTop = {
+      proximity: Math.abs(box.top - e.clientY),
+      side: "top",
+    };
+    const proximityToBottom = {
+      proximity: Math.abs(box.bottom - e.clientY),
+      side: "bottom",
+    };
+
+    const sortedProximity = [
+      proximityToLeft,
+      proximityToRight,
+      proximityToTop,
+      proximityToBottom,
+    ].sort((a, b) => a.proximity - b.proximity);
+
+    return sortedProximity[0].side;
+  };
+
+  const handleMouseEnter = (e) => {
+    const side = getNearestSide(e);
+
+    animate.start({
+      clipPath: ENTRANCE_KEYFRAMES[side],
+    });
+  };
+
+  const handleMouseLeave = (e) => {
+    const side = getNearestSide(e);
+
+    animate.start({
+      clipPath: EXIT_KEYFRAMES[side],
+    });
+  };
+
+  return (
+    <div>
+      <a
+        href={href}
+        onMouseEnter={(e) => handleMouseEnter(e)}
+        onMouseLeave={(e) => handleMouseLeave(e)}
+        className="relative grid h-20 w-full place-content-center sm:h-28 md:h-36"
+      >
+        <Icon className="text-xl sm:text-3xl lg:text-4xl" />
+
+        <motion.div
+          ref={scope}
+          initial={{ clipPath: BOTTOM_RIGHT_CLIP }}
+          animate={animate}
+          className="absolute inset-0 grid place-content-center bg-neutral-900 text-white"
+        >
+          <Icon className="text-xl sm:text-3xl md:text-4xl" />
+        </motion.div>
+      </a>
+    </div>
+  );
+};
+
+const FloatingText = () => {
+  const containerRef = useRef(null);
+  const textRef = useRef(null);
+  const controls = useAnimation();
+
+  useEffect(() => {
+    resizeText();
+    window.addEventListener('resize', resizeText);
+    return () => window.removeEventListener('resize', resizeText);
+  }, []);
+
+  const resizeText = () => {
+    const container = containerRef.current;
+    const text = textRef.current;
+
+    if (!container || !text) return;
+
+    const containerWidth = container.offsetWidth;
+    let min = 1;
+    let max = 2500;
+
+    while (min <= max) {
+      const mid = Math.floor((min + max) / 2);
+      text.style.fontSize = mid + 'px';
+
+      if (text.offsetWidth <= containerWidth) {
+        min = mid + 1;
+      } else {
+        max = mid - 1;
+      }
+    }
+
+    text.style.fontSize = max + 'px';
+
+    // Set the animation
+    // controls.start({
+    //   x: [containerWidth, -text.offsetWidth],
+    //   transition: {
+    //     duration: 10,
+    //     ease: 'linear',
+    //     repeat: Infinity,
+    //   },
+    // });
+  };
+
+  return (
+    <div className="flex w-full items-center overflow-hidden" ref={containerRef}>
+      <motion.span
+        className="bottom-0 left-0 mx-auto whitespace-nowrap text-center font-bold uppercase text-black"
+        ref={textRef}
+        animate={controls}
+      >
+        CONTACT US
+      </motion.span>
+    </div>
+  );
+};
+
+const Contact = () => {
+  return (
+    <div>
+      <FloatingText />
+      <div className="divide-y divide-neutral-900 border border-neutral-900">
+        <div className="grid grid-cols-2 divide-x divide-neutral-900">
+          <LinkBox Icon={SiGmail} href="#" />
+          <LinkBox Icon={SiLinkedin} href="#" />
+        </div>
+        <div className="grid grid-cols-4 divide-x divide-neutral-900">
+          <LinkBox Icon={SiInstagram} href="#" />
+          <LinkBox Icon={SiX} href="#" />
+          <LinkBox Icon={SiDiscord} href="#" />
+          <LinkBox Icon={SiTelegram} href="#" />
+        </div>
+        <div className="grid grid-cols-3 divide-x divide-neutral-900">
+          <LinkBox Icon={SiFacebook} href="#" />
+          <LinkBox Icon={SiYoutube} href="#" />
+          <LinkBox Icon={SiMedium} href="#" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -9,29 +9,42 @@ const Services = () => {
         imgUrl="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         subheading="What we do"
         heading="Services"
-      >
-        {/* <ExampleContent /> */}
-      </TextParallaxContent>
+      ></TextParallaxContent>
       <TextParallaxContent
         imgUrl="https://images.unsplash.com/photo-1530893609608-32a9af3aa95c?q=80&w=2564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         subheading="Modern"
         heading="WEB SOLUTIONS"
       >
-        <ExampleContent heading={"Crafting Digital Experiences"} desc={"Our Web Solutions service is all about crafting digital experiences that captivate and engage your audience. From intuitive user interfaces to seamless navigation, we create websites that not only look great but also drive results."} />
+        <Content
+          heading={"Crafting Digital Experiences"}
+          desc={
+            "Our Web Solutions service is all about crafting digital experiences that captivate and engage your audience. From intuitive user interfaces to seamless navigation, we create websites that not only look great but also drive results."
+          }
+        />
       </TextParallaxContent>
       <TextParallaxContent
         imgUrl="https://images.unsplash.com/photo-1504610926078-a1611febcad3?q=80&w=2416&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         subheading="Modern"
-        heading="MOBILE APP DEVELOPMENT"
+        heading="MOBILE APP SOLUTIONS"
       >
-        <ExampleContent heading={"Mobile Innovation Solution"} desc={"Unlock the potential of the Android + iOS platforms with our Mobile App Development service. We specialize in building high-performance, feature-rich mobile applications that offer a smooth and delightful user experience, helping you reach and engage your customers on their favorite devices."}/>
+        <Content
+          heading={"Mobile Innovation Solution"}
+          desc={
+            "Unlock the potential of the Android + iOS platforms with our Mobile App Development service. We specialize in building high-performance, feature-rich mobile applications that offer a smooth and delightful user experience, helping you reach and engage your customers on their favorite devices."
+          }
+        />
       </TextParallaxContent>
       <TextParallaxContent
         imgUrl="https://images.unsplash.com/photo-1504610926078-a1611febcad3?q=80&w=2416&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         subheading="BEST"
         heading="BRAND DEVELOPMENT"
       >
-        <ExampleContent heading={"Shaping Brand Identity"} desc={"With our Brand Development service, we help businesses shape and elevate their brand identity. From crafting compelling brand stories to designing impactful visual assets, we work closely with you to strengthen your brand presence and make a lasting impression in the market."}/>
+        <Content
+          heading={"Shaping Brand Identity"}
+          desc={
+            "With our Brand Development service, we help businesses shape and elevate their brand identity. From crafting compelling brand stories to designing impactful visual assets, we work closely with you to strengthen your brand presence and make a lasting impression in the market."
+          }
+        />
       </TextParallaxContent>
     </div>
   );
@@ -111,29 +124,24 @@ const OverlayCopy = ({ subheading, heading }) => {
       <p className="mb-2 text-center text-xl md:mb-4 md:text-3xl">
         {subheading}
       </p>
-      <p className="text-center text-4xl font-bold md:text-7xl">{heading}</p>
+      <p className="text-center text-5xl font-bold md:text-8xl lg:text-9xl">{heading}</p>
     </motion.div>
   );
 };
 
-const ExampleContent = ({heading, desc, desc2}) => (
+const Content = ({ heading, desc, desc2 }) => (
   <div className="mx-auto grid max-w-5xl grid-cols-1 gap-8 px-4 pb-24 pt-12 md:grid-cols-12">
     <h2 className="col-span-1 text-3xl font-bold md:col-span-4">
       {heading || "MORE TO EXPLORE"}
     </h2>
     <div className="col-span-1 md:col-span-8">
-      <p className="mb-4 text-xl text-neutral-600 md:text-2xl">
-        {desc}
-      </p>
-      <p className="mb-8 text-xl text-neutral-600 md:text-2xl">
-        {desc2}
-      </p>
+      <p className="mb-4 text-xl text-neutral-600 md:text-2xl">{desc}</p>
+      <p className="mb-8 text-xl text-neutral-600 md:text-2xl">{desc2}</p>
       <button className="w-full rounded bg-neutral-900 px-9 py-4 text-xl text-white transition-colors hover:bg-neutral-700 md:w-fit">
         Learn more <FiArrowUpRight className="inline" />
       </button>
     </div>
   </div>
 );
-
 
 export default Services;
